27b33b94db55748b6c31bcdc039a089cff66dd91
NateChris <divineconclusion@gmail.com>
2020-02-11 14:48:39 -0500

    Contact form first attempt created: errors importing form class

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this is Sparta!'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAILPORT'] = '465' # or 2525
+app.config['MAIL_USERNAME'] = 'c137704b5d02ff'
+app.config['MAIL_PASSWORD'] = 'c4712a5745c66f'
+
+mail = Mail(app)
 from app import views
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ This file creates your application.
 """
 
 from app import app
+from forms import ContactForm
 from flask import render_template, request, redirect, url_for, flash
 
 
@@ -24,6 +25,13 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact')
+def contact():
+    """Render the website's contact us page"""
+    form  = ContactForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('contact.html')
 
 ###
 # The functions below should be applicable to all Flask apps.
